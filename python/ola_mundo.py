9bf69d90c09f1306ead124fec966da7cec94d819
Henrique <90357661+LordeRike@users.noreply.github.com>
2026-09-11 19:53:20 -0300

    tipos de dados

--- python/ola_mundo.py
+++ python/ola_mundo.py
@@ -1,1 +1,45 @@
 print("Olá, Mundo!")
+
+"""
+Indentação
+No Python, a indentação (espaços ou tabulações no início de uma linha) é utilizada para delimitar blocos de código. Diferente de outras linguagens que utilizam chaves ou palavras-chave, o Python utiliza a indentação para determinar o escopo das declarações. Por exemplo: 
+
+if condition:
+    # Bloco de código se a condição for verdadeira
+    instrucao1
+    instrucao2
+else:
+    # Bloco de código se a condição for falsa
+    instrucao3
+    instrucao4
+"""
+
+"""
+Comentários
+Os comentários são linhas de texto no código que são ignoradas pelo interpretador do Python. Eles são utilizados para explicar ou documentar o código. No Python, os comentários de uma única linha começam com o símbolo #, enquanto os comentários de várias linhas são delimitados por três aspas ''
+. Por exemplo:
+# Este é um comentário de uma única linha
+
+Este é um comentário
+de várias linhas
+"""
+
+"""
+Sensibilidade a maiúsculas e minúsculas
+Python distingue entre maiúsculas e minúsculas.
+Portanto, variável, Variável e VARIÁVEL são consideradas variáveis diferentes.
+"""
+
+"""
+Ponto e vírgula
+Diferente de outras linguagens, o Python não requer o uso de ponto e vírgula (;) ao final de cada instrução. No entanto, se você desejar escrever várias instruções em uma única linha, pode separá-las com um ponto e vírgula.
+Por exemplo:
+instrucao1; instrucao2; instrucao3
+"""
+
+"""
+Uso de parênteses
+Os parênteses são utilizados para agrupar expressões, definir funções e realizar chamadas a funções.
+Por exemplo:
+resultado = (a + b) * c
+"""
